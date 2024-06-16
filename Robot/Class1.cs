097ad29452ab1233e007032727bddab178cde271
User <User@DESKTOP-21G3OB3>
2024-06-16 20:55:37 +0300

    Add project files.

---
+++ Robot/Class1.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Planet earth = new Planet("Earth");
+
+        
+        earth.AddTarget(new Target(TargetType.Animal));
+        earth.AddTarget(new Target(TargetType.Human));
+        earth.AddTarget(new Target(TargetType.Superhero));
+
+        
+        GiantKillerRobot robot = new GiantKillerRobot();
+
+        
+        robot.EyeLaserIntensity = Intensity.Kill;
+        robot.SetTargets(earth.Targets);
+
+        
+        robot.ExecuteMission(earth);
+    }
+}
---
+++ Robot/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Intensity
+{
+    Stun,
+    Kill
+}
+
+public enum TargetType
+{
+    Animal,
+    Human,
+    Superhero
+}
+
+public class Target
+{
+    public TargetType TargetType { get; private set; }
+    public bool IsAlive { get; private set; }
+
+    public Target(TargetType targetType)
+    {
+        TargetType = targetType;
+        IsAlive = true;
+    }
+
+    public void Eliminate()
+    {
+        IsAlive = false;
+    }
+}
+
+public class Planet
+{
+    public string Name { get; private set; }
+    public List<Target> Targets { get; private set; }
+
+    public Planet(string name)
+    {
+        Name = name;
+        Targets = new List<Target>();
+    }
+
+    public void AddTarget(Target target)
+    {
+        Targets.Add(target);
+    }
+
+    public bool ContainsLife()
+    {
+        return Targets.Any(target => target.IsAlive);
+    }
+}
+
+public class GiantKillerRobot
+{
+    public Intensity EyeLaserIntensity { get; set; }
+    public bool Active { get; private set; }
+    private List<Target> Targets { get; set; }
+    private int CurrentTargetIndex { get; set; }
+
+    public GiantKillerRobot()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        Active = true;
+        CurrentTargetIndex = 0;
+    }
+
+    public void SetTargets(List<Target> targets)
+    {
+        Targets = targets;
+    }
+
+    public void AcquireNextTarget()
+    {
+        CurrentTargetIndex++;
+        if (CurrentTargetIndex >= Targets.Count)
+        {
+            CurrentTargetIndex = 0;
+        }
+    }
+
+    public Target CurrentTarget()
+    {
+        return Targets[CurrentTargetIndex];
+    }
+
+    public void FireLaserAt(Target target)
+    {
+        if (EyeLaserIntensity == Intensity.Kill)
+        {
+            target.Eliminate();
+        }
+    }
+
+    public void ExecuteMission(Planet planet)
+    {
+        while (Active && planet.ContainsLife())
+        {
+            Target target = CurrentTarget();
+            if (target.IsAlive)
+            {
+                FireLaserAt(target);
+            }
+            else
+            {
+                AcquireNextTarget();
+            }
+        }
+    }
+}
